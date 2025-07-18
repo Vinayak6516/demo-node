2b10fe6b1d360b6851b34c4e9b87ec76039ce2cb
Vinayak Burbure <121487445+Vinayak6516@users.noreply.github.com>
2025-07-18 09:53:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const app = express();
 const PORT = 3000;
 var StatsD = require('hot-shots');
 var dogstatsd = new StatsD();
-
-// Increment a counter.
 dogstatsd.increment('page.views')
 
 app.get('/', (req, res) => {
- res.send('Hello World! This is my demo Node.js app.');
+ res.send("Hello World! This is my demo Node.js app for Better's DevOps Internship.");
 });
-// Start server
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 }); 

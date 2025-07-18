a7b477de9787a960c41172a7bf82ed5fb475ea5e
Ubuntu <ubuntu@ip-172-31-82-34.ec2.internal>
2025-07-18 04:17:48 +0000

    Added Datadog Monitoing

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+var StatsD = require('hot-shots');
+var dogstatsd = new StatsD();
 
-// Simple route
+// Increment a counter.
+dogstatsd.increment('page.views')
+
 app.get('/', (req, res) => {
  res.send('Hello World! This is my demo Node.js app.');
 });
